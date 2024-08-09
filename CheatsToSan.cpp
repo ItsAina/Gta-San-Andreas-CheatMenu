89f580aaec435f19bd823a6bcfbf057fa27e49f6
NotAina <145102109+ItsAina@users.noreply.github.com>
2024-08-08 18:58:22 -0700

    Update CheatsToSan.cpp

--- CheatsToSan.cpp
+++ CheatsToSan.cpp
@@ -18,6 +18,15 @@ using namespace std;
 //
 //
 //};
+
+std::string toLower(const std::string& str) {
+	std::string lowerStr = str; // Make a copy of the input string
+	std::transform(lowerStr.begin(), lowerStr.end(), lowerStr.begin(),
+		[](unsigned char c) { return std::tolower(c); });
+	return lowerStr;
+}
+
+
 DWORD getbasemoduleaddress(const wchar_t* modulename, DWORD pid) {
 	DWORD dwModuleBaseAddress = 0;
 	MODULEENTRY32 ModuleEntry32 = { 0 };
@@ -55,28 +64,58 @@ void choosecheatmenumode(DWORD pid,HANDLE GetHandle) {
 	vector<DWORD> offsets = { 0x540,0xB4,0x0,0x4,0xB0,0x1CC };
 	vector<DWORD> locationoffsets = { 0x30,0x34,0x38 };
 	//float health = *(float*)(0xB6F5F0 + 0x540);
-	DWORD MoneyOffset = 0xB7F4D0;
+	DWORD MoneyAddr = 0xB7F4D0;
+	DWORD FatAddr = 0xB7BA54;
+	DWORD StaminaAddr = 0xB7BA58;
+	DWORD MuscleAddr = 0xB7BA5C;
+	DWORD LungCapacityAddr = 0xB7F43C;
+	DWORD MaxHealthAddr = 0xB7A60;
+	DWORD RespectAddr = 
+	DWORD Inventory = 0xB7F43C;
+	DWORD TimeControl = 0xB7F4D0;
 	DWORD BaseAddr = getbasemoduleaddress(L"gta_sa.exe", pid);
 	DWORD PlayerObj = 0x771A38;
 	DWORD xaddr = 0x771C70;
 	DWORD yaddr = 0x771A38;
 	DWORD zaddr = 0x771C70;
+	DWORD skillpistol = 0xB7BB14;
+	DWORD skillsilenecedpistol = 0xB7BB18;
+	DWORD skillDeagle = 0xB7BB1C;
+	DWORD skillshotgun = 0xB7BB20;
+	DWORD skillsawn = 0xB7BB24;
+	DWORD skillcombatshotgun = 0xB7BB28;
+	DWORD skillmachinepistol = 0xB7BB2C;
+	DWORD skillsmg = 0xB7BB30;
+	DWORD skillak47 = 0xB7BB34;
+	DWORD skillm4 = 0xB7BB38;
+	DWORD SpawnRom = 0xB7B6C4;
+
+
+
+	void* PlayerObject = (void*)(BaseAddr + PlayerObj);
+	void* xobj = (void*)(BaseAddr + xaddr);
+	void* yobj = (void*)(BaseAddr + yaddr);
+	void* zobj = (void*)(BaseAddr + zaddr);
 	uintptr_t Playerptr;
 	uintptr_t xptr;
 	uintptr_t yptr;
 	uintptr_t zptr;
-	ReadProcessMemory(GetHandle, LPCVOID(BaseAddr + PlayerObj), &Playerptr, sizeof(Playerptr), 0);
-	ReadProcessMemory(GetHandle, LPCVOID(BaseAddr + xaddr), &xptr, sizeof(xptr), 0);
-	ReadProcessMemory(GetHandle, LPCVOID(BaseAddr + yaddr), &yptr, sizeof(yptr), 0);
-	ReadProcessMemory(GetHandle, LPCVOID(BaseAddr + zaddr), &zptr, sizeof(zptr), 0);
+
+	ReadProcessMemory(GetHandle, PlayerObject, &Playerptr, sizeof(Playerptr), 0);
+	ReadProcessMemory(GetHandle, xobj, &xptr, sizeof(xptr), 0);
+	ReadProcessMemory(GetHandle, yobj, &zptr, sizeof(zptr), 0);
+	ReadProcessMemory(GetHandle, zobj, &yptr, sizeof(yptr), 0);
+
+	
 	//DWORD Offset = getpointeraddress(getwindowtitle,PlayerObjectAddr,offsets);
 	bool godmode = false;
-	int Addmoney;
+	int money;
 	float health, armor;
-	float fatness,stamina,muscle;
+	float maxhealth;
+	float fatness,stamina,muscle,lung,respect;
 	float x, y, z;
+	cout << "For toggle on godmode press: F2\t For changing money press: F4\nFor changing xyz position press: F5\t For infinity armor press: F6\nFor altering stats press: F7\t For toggle on aimbot press: F8\n ";
 	while (true) {
-		
 		if (GetAsyncKeyState(VK_F2)) {
 			godmode = not godmode;
 			this_thread::sleep_for(chrono::milliseconds(500));
@@ -98,25 +137,17 @@ void choosecheatmenumode(DWORD pid,HANDLE GetHandle) {
 
 			}
 		}
-		if (GetAsyncKeyState(VK_F3)) {
-			ReadProcessMemory(GetHandle, LPCVOID(MoneyOffset), &Addmoney, sizeof(Addmoney), 0);
-			cout << "Your current money is" << "$" << Addmoney << endl;
-			cout << "How much money, you want to add?" << endl;
-			cin >> Addmoney;
-			WriteProcessMemory(GetHandle, LPVOID(MoneyOffset), &Addmoney, sizeof(Addmoney), 0);
-
-
-		}
-		if (GetAsyncKeyState(VK_F4)) {
+	
+		if (GetAsyncKeyState(VK_F5)) {
 			ReadProcessMemory(GetHandle, LPCVOID(xptr + 0x30), &x, sizeof(x), 0);
 			ReadProcessMemory(GetHandle, LPCVOID(yptr + 0x34), &y, sizeof(y), 0);
 			ReadProcessMemory(GetHandle, LPCVOID(zptr + 0x38), &z, sizeof(z), 0);
-			cout << "Your current position of:\t" << "x\t" << x << "y\t" << y << "z\t" << z << "\n";
+			cout <<"Your current position of:\t" << "x:" << x << "\t" << "y:" << y << "\t" << "z:" << z << "\n";
 			cout << "Add coordinates for x" << endl;
 			cin >> x;
 			cout << "Add coordinates for y" << endl;
 			cin >> y;
-			cout << "Add coordinates for x" << endl;
+			cout << "Add coordinates for z" << endl;
 			cin >> z;
 
 
@@ -129,6 +160,73 @@ void choosecheatmenumode(DWORD pid,HANDLE GetHandle) {
 				counter += 1;
 
 			}*/
+
+		}
+
+		if (GetAsyncKeyState(VK_F7)) {
+			//cout << ""
+			string UserInput;
+			cin >> UserInput;
+			this_thread::sleep_for(chrono::milliseconds(500));
+			if (toLower(UserInput) == "muscle") {
+				ReadProcessMemory(GetHandle, LPCVOID(MuscleAddr), &muscle, sizeof(muscle), 0);
+				cout << "Your current muscle stat is" << muscle << endl;
+
+			}
+
+			else if (toLower(UserInput) == "stamina") {
+				ReadProcessMemory(GetHandle,LPCVOID(StaminaAddr), &stamina, sizeof(stamina), 0);
+				cout << "Your current stamina stat is" << muscle << endl;
+
+			}
+			else if (toLower(UserInput) == "fat") {
+				ReadProcessMemory(GetHandle, LPCVOID(FatAddr), &fatness, sizeof(fatness), 0);
+				cout << "Your current fat stat is" << muscle << endl;
+
+			}
+			else if (toLower(UserInput) == "lung") {
+				ReadProcessMemory(GetHandle, LPCVOID(LungCapacityAddr), &lung, sizeof(lung), 0);
+				cout << "Your current lung stat is" << muscle << endl;
+			}
+
+			else if (toLower(UserInput) == "maxhealth") {
+				ReadProcessMemory(GetHandle, LPCVOID(MaxHealthAddr), &maxhealth, sizeof(maxhealth), 0);
+				cout << "Your current maxhealth stat is" << muscle << endl;
+
+			}
+
+
+			else if (toLower(UserInput) == "money") {
+				ReadProcessMemory(GetHandle, LPCVOID(MoneyAddr), &money, sizeof(money), 0);
+				cout << "Your current money is" << "$" << money << endl;
+				cout << "How much money, you want to add?" << endl;
+				cin >> money;
+				WriteProcessMemory(GetHandle, LPVOID(MoneyAddr), &money, sizeof(money), 0);
+
+			}
+
+			else if (toLower(UserInput) == "respect") {
+				ReadProcessMemory(GetHandle, LPCVOID();
+				cout << "Your current respect stat is" << muscle << endl;
+
+			}
+
+			/*	else if (toLower(UserInput) == "weaponskill") {
+					ReadProcessMemory();
+					cout << "Your current muscle stat is" << muscle << endl;
+
+				}
+
+				else if (toLower(UserInput) == "driveskill") {
+					ReadProcessMemory();
+					cout << "Your current muscle stat is" << muscle << endl;*/
+
+			//}
+
+
+
+
+
 
 		}
 
